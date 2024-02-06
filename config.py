229d0f6a9b9e1453664fd4f002fdbc1543e0e58a
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2024-02-06 13:32:35 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5814400363:AAFNi6x2Qdu9KGKOdBET9aCNQc0iMKXlydw")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5901290128:AAFWU9kP-2Lw9jaCpDZU6GwuxD-OyZFbcj8")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "26730559"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "26730559"))
 API_HASH = os.environ.get("API_HASH", "54e0fd326f54b4ea91fdcbdf98e3cf4e")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001933776086"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001809853987"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "Dhilnihnge")
@@ -36,11 +36,11 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://pvwynzrg:4zdeSw6H57IK5kUgQv4Fcz00-1K3mr52@flora.db.elephantsql.com/pvwynzrg")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://rceqsgsy:p2F_zfuY-YNGOwuxEiv7EvUAl8Cy6Gr7@flora.db.elephantsql.com/rceqsgsy")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001693649358"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001897080662"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001609714643"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001271076945"))
 
 # Tulisan joinnya mau gimana
 BUTTONS_JOIN_TEXT = os.environ.get("BUTTONS_JOIN_TEXT", "ᴊᴏɪɴ ᴅᴜʟᴜ ꜱᴇʙᴇʟᴜᴍ ɴᴏɴᴛᴏɴ")
